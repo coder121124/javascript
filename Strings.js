fd2a5f365544cd07da66630fe96f55818fa553e4
Sedaji <44921067+sedaji@users.noreply.github.com>
2023-07-17 06:39:58 -0500

    Update Strings.js

--- Strings.js
+++ Strings.js
@@ -1,3 +1,5 @@
-// Practice alert and interpolation
+// Practice alert, best practices for converting dollars into cents first before calculations, and interpolations. :)
 alert(`Total cost: $${(599+295)/100}
 Thank you! Come again!`)
+// P.S. Function + ; = Home
+// Function + . = End
