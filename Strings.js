1338a77d553e679c87d59c33450cdfb604ab5e19
Sedaji <44921067+sedaji@users.noreply.github.com>
2023-07-17 07:13:43 -0500

    Update Strings.js

--- Strings.js
+++ Strings.js
@@ -3,3 +3,9 @@ alert(`Total cost: $${(599+295)/100}
 Thank you! Come again!`)
 // P.S. Function + ; = Home
 // Function + . = End
+
+`Items (${2+2}): $${ (2095*2 + 799*2) / 100}`
+// 'Items (4): $57.88'
+
+`Total before tax: $${Math.round((998+ 5788)*.1) / 100}`
+// 'Total before tax: $6.79'
